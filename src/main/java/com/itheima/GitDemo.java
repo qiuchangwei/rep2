eceb8c407eb5baac00fdec13631c31ed5bcfcab4
qiuchangwei <593988693@qq.com>
2018-10-28 19:48:47 +0800

    提交到本地仓库

---
+++ src/main/java/com/itheima/GitDemo.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+    }
+}
